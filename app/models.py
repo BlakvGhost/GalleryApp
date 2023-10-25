8133c51f0868ef89baec1b9cbf0eb4199f028deb
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-10-25 16:14:00 +0100

    Template: Added a grid display of albums

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Album(models.Model):
+    slug = models.CharField(max_length=100, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.slug
+    
+class Photo(models.Model):
+    slug = models.CharField(max_length=100, null=False)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.slug
--- app/views.py
+++ app/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Album, Photo
 
 @login_required
 def default(request):
     
-    return render(request, 'app/index.html')
+    albums = Album.objects.all().order_by('-id')
+    
+    context = {
+        'albums': albums,
+    }
+    
+    return render(request, 'app/index.html', context)
 
 
 @login_required
 def publish(request):
     
     return render(request, 'app/publish.html')
+
+
+@login_required
+def album(request):
+    
+    slug = request.POST.get('slug')
+    
+    if slug:
+        if request.method == 'POST':
+            album = Album.objects.create(
+                slug=slug,
+                user=request.user
+            )
+        
+    
+    return redirect('default')
 
