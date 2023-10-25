4f07373650f575c26a6d8abd0ab49054106368bb
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-10-25 14:16:08 +0100

    Template: End of Publish Page

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.default, name="default"),
+    path('publish', views.publish, name="publish"),
 ]
--- app/views.py
+++ app/views.py
@@ -5,3 +5,10 @@ from django.contrib.auth.decorators import login_required
 def default(request):
     
     return render(request, 'app/index.html')
+
+
+@login_required
+def publish(request):
+    
+    return render(request, 'app/publish.html')
+
