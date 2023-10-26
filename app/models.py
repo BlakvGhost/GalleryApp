8bd7415bdf07cb0a7930ecec5fcbf0282c476861
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-10-26 12:19:38 +0100

    Added the ability to create thumbnails for each uploaded image

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from slugify import slugify
-
+from PIL import Image
 
 class Album(models.Model):
     slug = models.CharField(max_length=100, null=False)
@@ -9,14 +9,15 @@ class Album(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.slug
-    
+
 def user_directory_path(instance, filename):
     user = instance.album.user
     album_name = instance.album.slug
     username = f"{user.id} {user.first_name} {user.last_name}"
-    return f'{username}/{album_name}/{slugify(filename)}'
+    original_name = filename
+    return f'{username}/{album_name}/{slugify(original_name)}'
 
 class Photo(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='photos')
@@ -25,5 +26,20 @@ class Photo(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.original_name
+
+    def save(self, *args, **kwargs):
+        super(Photo, self).save(*args, **kwargs)
+
+        image = Image.open(self.image.path)
+
+        thumbnail_size = (100, 100)
+
+        thumbnail = image.copy()
+        thumbnail.thumbnail(thumbnail_size)
+        thumbnail_path = self.image.path.replace(".jpg", "_thumbnail.jpg")
+        thumbnail.save(thumbnail_path)
+
+    def create_thumbnail(self):
+        pass
